99aa89cc3196601f9ab6cf7a5c55f136d8e1ec15
taewon0 <ktw495@naver.com>
2026-04-01 11:52:30 +0900

    Refactor: null 검증 추가

--- src/main/java/com/nudgebank/paymentbackend/common/exception/BusinessException.java
+++ src/main/java/com/nudgebank/paymentbackend/common/exception/BusinessException.java
@@ -2,13 +2,15 @@ package com.nudgebank.paymentbackend.common.exception;
 
 import lombok.Getter;
 
+import java.util.Objects;
+
 @Getter
 public class BusinessException extends RuntimeException {
 
     private final ErrorCode errorCode;
 
     public BusinessException(ErrorCode errorCode) {
-        super(errorCode.getMessage());
+        super(Objects.requireNonNull(errorCode, "errorCode must not be null").getMessage());
         this.errorCode = errorCode;
     }
 }
